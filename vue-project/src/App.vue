8c70622dbed1b7fda5711828fe90730317b0a96f
Hiroya Yabiku <h_yabiku@atoj.co.jp>
2020-05-18 19:48:55 +0900

    six type of transition classies

--- vue-project/src/App.vue
+++ vue-project/src/App.vue
@@ -1,5 +1,10 @@
 <template>
-  <div id="app">
+  <div id="app" class="main">
+
+    <button @click="show = !show">切り替え</button>
+    <transition name="fade">
+      <p v-if="show">Hello</p>
+    </transition>
 
   </div>
 </template>
@@ -7,17 +12,48 @@
 <script>
 
 export default {
+  data() {
+    return {
+      show: true
+    }
+  },
 
 }
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+<style scoped>
+.fade-enter {
+  /* 現れる時の最初の状態 */
+  opacity: 0;
+
 }
+.fade-enter-active {
+  /* 現れる時のトランジションの状態 */
+  transition: opacity .5s;
+
+}
+.fade-enter-to {
+  /* 現れる時の最後の状態 */
+  opacity: 1;
+
+}
+.fade-leave {
+  /* 消える時の最初の状態 */
+  opacity: 1;
+}
+.fade-leave-active {
+  /* 消える時のトランジションの状態 */
+  transition: opacity .5s;
+}
+.fade-leave-to {
+  /* 消える時の最後の状態 */
+  opacity: 0;
+}
+
+.main {
+  width: 70%;
+  margin: 5rem auto;
+  text-align: center;
+}
+
 </style>
